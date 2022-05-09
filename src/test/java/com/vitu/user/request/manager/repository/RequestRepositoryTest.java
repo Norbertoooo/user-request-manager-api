08c67990bce9a984a74d1824819c1c489b422337
Vítor Norberto <vithor.norberto@gmail.com>
2022-05-08 23:56:08 -0300

    Corrige teste de unidade

--- src/test/java/com/vitu/user/request/manager/repository/RequestRepositoryTest.java
+++ src/test/java/com/vitu/user/request/manager/repository/RequestRepositoryTest.java
@@ -4,8 +4,8 @@ import com.vitu.user.request.manager.domain.Request;
 import com.vitu.user.request.manager.domain.RequestState;
 import com.vitu.user.request.manager.domain.Role;
 import com.vitu.user.request.manager.domain.User;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -16,8 +16,10 @@ import static com.vitu.user.request.manager.AbstractTest.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@Slf4j
 @DataJpaTest
 @ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class RequestRepositoryTest {
 
     @Autowired
@@ -33,6 +35,7 @@ class RequestRepositoryTest {
     }
 
     @Test
+    @Order(1)
     void findByUser_Id() {
         User userOne = createUser(NAME, PASSWORD, EMAIL, Role.SIMPLE);
         User userTwo = createUser("userTwo", PASSWORD, "userTwo@gmail.com", Role.SIMPLE);
@@ -48,11 +51,15 @@ class RequestRepositoryTest {
     }
 
     @Test
+    @Order(2)
     void updateStatus() {
-        Request request = createRequest("description", "subject", null, RequestState.OPEN);
-        requestRepository.save(request);
+        User userOne = createUser(NAME, PASSWORD, EMAIL, Role.SIMPLE);
+        User save = userRepository.save(userOne);
+        Request request = createRequest("description", "subject", save, RequestState.OPEN);
+        Request savedRequest = requestRepository.save(request);
+        log.info("{}", savedRequest);
 
-        int effectedRows = requestRepository.updateStatus(1L, RequestState.CLOSED.name());
+        int effectedRows = requestRepository.updateStatus(2L, RequestState.CLOSED.name());
 
         assertEquals(1, effectedRows);
     }
